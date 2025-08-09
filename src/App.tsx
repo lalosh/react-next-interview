ff5744c0d2416dbe77f17ab0530c7392b1caee4b
lalosh <lalosh.dev@gmail.com>
2025-08-09 18:33:27 +0300

    Start with storing the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,63 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 
-/*
- task1: add types for the response
- this is how a single todo object looks like
-      {
-        "id": 1,
-        "todo": "Do something nice for someone you care about",
-        "completed": false,
-        "userId": 152
-      }
+/**
+ * Task: store the fetched todos and render them
  */
 
-// task2: add a new function that can fetch single todo
-// note:
-// to get single todo call the API
-// https://dummyjson.com/todos/{id}
+/*
 
-function getTodos() {
+Todo object shape:
+
+{
+  "id": 1,
+  "todo": "Do something nice for someone you care about",
+  "completed": false,
+  "userId": 152
+}
+
+*/
+
+function fetchTodos() {
   return fetch("https://dummyjson.com/todos")
     .then((res) => res.json())
     .then((r) => r.todos);
 }
 
-function markTodoAsDone() {
-  return fetch("https://dummyjson.com/todos/1", {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      completed: true,
-    }),
-  }).then((res) => res.json());
-}
-
 function App() {
-  const [todos, setTodos] = useState([]);
+  const todos: any[] = [];
 
   useEffect(function onLoad() {
-    getTodos().then((response) => {
+    fetchTodos().then((response) => {
       console.log({ response });
-      setTodos(response);
+      // store todos
     });
   }, []);
 
   return (
-    <>
-      <table>
-        {todos?.map((item) => (
-          <tr key={item.id}>
-            <td>
-              <span>{item.id}</span>
-            </td>
-            <td style={{ textAlign: "left" }}>
-              <span>{item.todo}</span>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  if (!item.completed) {
-                    markTodoAsDone();
-                  }
-                }}
-              >
-                {!item.completed ? "Done" : "Undo"}
-              </button>
-            </td>
-          </tr>
-        ))}
-      </table>
-    </>
+    <table>
+      {todos?.map((item) => (
+        <tr key={item.id}>
+          <td>
+            <span>{item.id}</span>
+          </td>
+          <td style={{ textAlign: "left" }}>
+            <span>{item.todo}</span>
+          </td>
+          <td>
+            <button
+              onClick={() => {
+                if (!item.completed) {
+                  markTodoAsDone();
+                }
+              }}
+            >
+              {!item.completed ? "Done" : "Undo"}
+            </button>
+          </td>
+        </tr>
+      ))}
+    </table>
   );
 }
 
